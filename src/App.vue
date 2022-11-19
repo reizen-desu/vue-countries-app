5d070c78afc47a753feb85bdbaf1d536bd2ce831
reizen-desu <nokiaq80@gmail.com>
2022-11-19 19:17:47 +0200

    feat: add footer to all pages

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
   <div id="app" class="app-wrapper" :class="{ 'app-wrapper--dark': isDark }">
     <navbar />
     <router-view />
+    <footer>
+      <div>
+        <h4>Reizen Dos Santos - DAW 2022</h4>
+      </div>
+    </footer>
   </div>
 </template>
 
